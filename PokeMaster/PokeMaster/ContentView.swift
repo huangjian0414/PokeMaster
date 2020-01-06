bf972c2b7b9dd3e8a7f3ea37d7ec62ae9fa5b04f
huangjian <huangjian@lanyife.com>
2020-01-06 15:31:37 +0800

    init  添加数据json ， Model ,Util等等

---
+++ PokeMaster/PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by huangjian on 2020/1/6.
+//  Copyright © 2020 huangjian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
